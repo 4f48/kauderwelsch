ac1d6013f202ed39a55249c32657e75fac8c74f8
4f48 <4f48>
2024-02-27 18:35:05 +0100

    Init

---
+++ src/components/generator.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onMount } from 'svelte';
+	import { QRCodeSVG } from '@akamfoad/qrcode';
+	let qrSVG;
+	let data;
+	function genQR() {
+		qrSVG = new QRCodeSVG(data);
+	};
+	let width = 100;
+</script>
+
+<body>
+	<input type="text" bind:value={data} on:input={genQR}/>
+	<svg {width}>
+		{@html qrSVG}
+	</svg>
+</body>
