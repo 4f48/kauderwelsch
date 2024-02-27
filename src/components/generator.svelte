388935e87f6b3ef9bfa572fe54bdc50de5a48935
4f48 <0x4f48@proton.me>
2024-02-27 20:11:14 +0100

    Progress

--- src/components/generator.svelte
+++ src/components/generator.svelte
@@ -1,17 +1,36 @@
 <script>
-	import { onMount } from 'svelte';
-	import { QRCodeSVG } from '@akamfoad/qrcode';
+	import { QRCodeSVG } from "@akamfoad/qrcode";
 	let qrSVG;
 	let data;
+	let color;
 	function genQR() {
-		qrSVG = new QRCodeSVG(data);
-	};
+		qrSVG = new QRCodeSVG(data, {
+			bgColor: color
+		});
+	}
 	let width = 100;
 </script>
 
-<body>
-	<input type="text" bind:value={data} on:input={genQR}/>
-	<svg {width}>
-		{@html qrSVG}
-	</svg>
-</body>
+<form class="flex items-center">
+	<fieldset name="QR code settings" class="flex flex-col">
+		<input
+			type="text"
+			placeholder="Data"
+			bind:value={data}
+			on:input={genQR}
+			class="border border-gray-300 drop-shadow-md focus:border-gray-600 focus:outline-none m-2 p-1 rounded-lg shadow-gray-300 text-sm"
+		/>
+		<input
+			type="text"
+			placeholder="Background colour"
+			bind:value={color}
+			on:input={genQR}
+			class="border border-gray-300 drop-shadow-md focus:border-gray-600 focus:outline-none m-2 p-1 rounded-lg shadow-gray-300 text-sm"
+		/>
+	</fieldset>
+	<output class="p-3 border border-black m-8 rounded-lg">
+		<svg {width} height={width}>
+			{@html qrSVG}
+		</svg>
+	</output>
+</form>
