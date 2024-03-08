6d3e26e24814d05b002a78695335fbcf5d64364d
4f48 <0x4f48@proton.me>
2024-03-08 14:38:03 +0100

    Add about pages, prepare for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind(), sitemap()],
+  site: 'https://kauderwelsch.vercel.app',
+  output: "server",
+  adapter: vercel()
 });
