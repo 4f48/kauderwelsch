9a255dbde6a413e7707ed1c92e7b4f56042dd453
4f48 <0x4f48@proton.me>
2024-03-08 14:55:23 +0100

    Try Speed Insights with Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), sitemap()],
+  integrations: [svelte(), tailwind(), sitemap(), partytown()],
   site: 'https://kauderwelsch.vercel.app',
   output: "server",
   adapter: vercel(),
-  prefetch: true,
+  prefetch: true
 });
