f7227048ed9226e0565e4185bd4a07766a9842d6
4f48 <0x4f48@proton.me>
2024-03-08 14:59:04 +0100

    Try again, without Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import sitemap from "@astrojs/sitemap";
 
-import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), sitemap(), partytown()],
+  integrations: [svelte(), tailwind(), sitemap()],
   site: 'https://kauderwelsch.vercel.app',
   output: "server",
   adapter: vercel(),
-  prefetch: true
+  prefetch: true,
 });
